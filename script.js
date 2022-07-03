bf09062073e47e1318eab19fd9d1e139bfaf41fb
Hayley Ashby <hayleyashby7@gmail.com>
2022-07-03 14:20:31 +0100

    Book class and basic Library array added

---
+++ script.js
@@ -0,0 +1,20 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+	let newBook = new Book(title, author, pages, read);
+
+	myLibrary.push(newBook);
+}
+
+addBookToLibrary("Nineteen Eighty-Four", "George Orwell", "328", "True");
+addBookToLibrary("Alice's Adventures in Wonderland", "Lewis Carroll", "320", "True");
+addBookToLibrary("Wuthering Heights", "Emily Bronte", "464", "False");
+
+console.log(myLibrary);
