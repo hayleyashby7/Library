e59dedba8538c26cb5c8d298da29a7188ce08bdf
Hayley Ashby <hayleyashby7@gmail.com>
2022-07-27 12:24:41 +0100

    Removed placeholder books

--- scripts/library.js
+++ scripts/library.js
@@ -16,10 +16,6 @@ function updateLibrary() {
 	}
 }
 
-addBookToLibrary("Nineteen Eighty-Four", "George Orwell", "328", true);
-addBookToLibrary("Alice's Adventures in Wonderland", "Lewis Carroll", "320", true);
-addBookToLibrary("Wuthering Heights", "Emily Bronte", "464", false);
-updateLibrary();
 formSetUp();
 
 export { myLibrary, addBookToLibrary, updateLibrary };
